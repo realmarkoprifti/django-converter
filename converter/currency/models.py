d7e7c67a8024ec6d2818ecc18f24d063e0afa57e
mPenTests <marko.prifti4@gmail.com>
2022-11-29 17:57:41 +0100

    Final Result

--- converter/currency/models.py
+++ converter/currency/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Coin(models.Model):
     id = models.CharField(max_length=5, primary_key=True)
     name = models.CharField(max_length=70)
-    symbol = models.CharField(max_length=5)
+    symbol = models.CharField(max_length=5, blank=True)
     
     def __str__(self):
         return f"{self.id}, {self.name}"
--- converter/currency/views.py
+++ converter/currency/views.py
@@ -1,49 +1,40 @@
 from django.shortcuts import render
 from decouple import config
+from .models import Coin
+import requests
 
 API_KEY = config("API_KEY")
 
 # Create your views here.
 
 def index(request):
-    return render(request, "currency/index.html", {
-        "input":'''<form action="" class="col-4">
-                    <div class="row">
-
-                        <label for="Amount" class="col-12"><b>Amount</b></label><br>
-
-                        <div class="col-2"></div>
-                        <input  class="col-8" type="text" id="Amount"><br><br>
-                        <div class="col-2"></div>
-
-                        <label for="from" class="col-12"><b>From</b></label><br>
-
-                        <div class="col-2"></div> 
-                        <select class="col-8">
-                            <option value="EU">EU</option>
-                            <option value="EU">EU</option>
-                            <option value="EU">EU</option>
-                            <option value="EU">EU</option>
-                        </select><br><br>
-                        <div class="col-2"></div>
-
-                        <label for="to" class="col-12"><b>To</b></label><br>
-
-                        <div class="col-2"></div>
-
-                        <select class="col-8">
-                            <option value="EU">EU</option>
-                            <option value="EU">EU</option>
-                            <option value="EU">EU</option>
-                            <option value="EU">EU</option>
-                        </select><br><br>
-
-                        <div class="col-2"></div>
-
-                        <div class="col-3"></div>
-                        <input class="col-6" id="convert" type="button" value="Convert">
-                        <div class="col-3"></div>
-
-                    </div>
-                </form>'''
-    })
+    if request.method == "GET":
+        
+        return render(request, "currency/index.html", {
+            "options": Coin.objects.all()
+        })
+        
+    elif request.method == "POST":
+        url = "https://free.currconv.com/api/v7/convert?q=USD_PHP,PHP_USD&compact=ultra&apiKey=" + API_KEY
+        try:
+            amount = float(request.POST["amount"])
+            
+        except:
+            return render(request, "currency/index.html", {
+            "options": Coin.objects.all()
+        })
+              
+        frm = request.POST["from"]
+        to = str(request.POST["to"])
+        frmlist = frm.split(', ')
+        tolist = to.split(', ')
+        url = str(url).replace("USD_PHP,PHP_USD", f"{frmlist[0]}_{tolist[0]}")
+        response = requests.get(url)
+        json = response.json()
+        number = float(json[f"{frmlist[0]}_{tolist[0]}"]) * amount
+        two_point_num = '{:.2f}'.format(number) 
+        final = f"{float(two_point_num):,}"
+        
+        return render(request, "currency/result.html", {
+            "result": f"{str(final)} {tolist[0]}"
+        })
